acfd3c487607c28c20e8f0da105ae77091c3c069
Lacy <44073703+lacyrich@users.noreply.github.com>
2023-02-15 18:27:54 -0600

    finite

---
+++ SP2023-Assignment2-lrichardson/SP2023-Assignment2-lrichardson/obj/Release/net6.0/PubTmp/Out/wwwroot/script.js
@@ -0,0 +1,117 @@
+var len;
+var results = '';
+
+//const timeButton = document.getElementById('#timeButton');
+
+function apiSearch() {
+  var params = {
+    "q": $("#query").val(),
+    "count": "50",
+    "offset": "0",
+    "mkt": "en-us"
+  };
+
+  $.ajax({
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+      beforeSend: function (xhrObj) {
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1d86a4de538b4df0bcb3f18620b14169");
+      },
+
+      type: "GET",
+    })
+    .done(function (data) {
+      len = data.webPages.value.length;
+      for (i = 0; i < len; i++) {
+        results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+      }
+
+      $('#searchResults').html(results);
+      $('#searchResults').dialog();
+    })
+    .fail(function () {
+      alert("error");
+    });
+}
+
+function apiLucky() {
+    var params = {
+        "q": $("#query").val(),
+        "count": "1",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1d86a4de538b4df0bcb3f18620b14169");
+        },
+
+        type: "GET",
+    })
+        .done(function (data) {
+            if (data.webPages.value.length > 0) {
+                window.location = data.webPages.value[0].url;
+            } else {
+                alert("No results found");
+            }
+        })
+        .fail(function () {
+            alert("error");
+        });
+}
+
+document.getElementById("feelingLuckyButton").addEventListener("click", function () {
+    apiLucky();
+});
+
+document.getElementById("searchButton").addEventListener("click", function () {
+    apiSearch();
+});
+
+
+let backgroundImageIndex = 0;
+const backgroundImages = ['url("day.jpg")','url("afternoon.jpg")', 'url("evening.jpg")'];
+
+document.getElementById("siteName").addEventListener("click", function () {
+    changeBackgroundImage();
+});
+
+function changeBackgroundImage() {
+    backgroundImageIndex = (backgroundImageIndex + 1) % backgroundImages.length;
+    document.body.style.backgroundImage = backgroundImages[backgroundImageIndex];
+    document.body.style.color = 'white';
+}
+
+document.getElementById("timeButton").addEventListener("click", function () {
+   showTime();
+});
+
+function showTime() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const formattedTime = formatTime(hours, minutes);
+
+    $('#time').text(formattedTime);
+    $('#time').dialog({
+        title: 'Current Time'
+    });
+
+}
+
+function formatTime(hours, minutes) {
+    let formatHours = hours;
+    let formatMinutes = minutes;
+
+    if (hours < 10) {
+        formatHours = `0${hours}`;
+    }
+
+    if (minutes < 10) {
+        formatMinutes = `0${minutes}`;
+    }
+
+    return `${formatHours}:${formatMinutes}`;
+}
+
